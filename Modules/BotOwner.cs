647406029ffdc3021a220bd3f1ee7af4c92708b6
therealswitzerwicki <therealswitzerwicki@gmail.com>
2022-02-03 20:25:19 +0000

    i made it not bad

--- Modules/BotOwner.cs
+++ Modules/BotOwner.cs
@@ -11,10 +11,10 @@ public class BotOwner : ModuleBase<SocketCommandContext>
 {
     public CommandService Commands { get; set; }
 
-    [Alias("botban")]
-    [Command("blacklist")]
+    [Alias("bot1984")]
+    [Command("denylist")]
     [Summary("Ban a user from using the bot.")]
-    [Remarks("$blacklist [user]")]
+    [Remarks("$denylist [user]")]
     public async Task<RuntimeResult> Blacklist(IGuildUser user)
     {
         if (user.IsBot)
@@ -26,30 +26,22 @@ public class BotOwner : ModuleBase<SocketCommandContext>
         return CommandResult.FromSuccess();
     }
 
-    [Command("cleartextchannel")]
+    [Command("1984channel")]
     [Summary("Deletes and recreates a text channel, effectively wiping its messages.")]
-    [Remarks("$cleartextchannel [channel]")]
+    [Remarks("$1984channel [channel]")]
     public async Task ClearTextChannel(ITextChannel channel)
     {
         await channel.DeleteAsync();
-        await Context.Guild.CreateTextChannelAsync(channel.Name, properties => {
-            properties.CategoryId = channel.CategoryId;
-            properties.IsNsfw = channel.IsNsfw;
-            properties.Name = channel.Name;
-            properties.PermissionOverwrites = new Optional<IEnumerable<Overwrite>>(channel.PermissionOverwrites.AsEnumerable());
-            properties.Position = channel.Position;
-            properties.SlowModeInterval = channel.SlowModeInterval;
-            properties.Topic = channel.Topic;
         });
     }
 
-    [Command("disablecmd")]
+    [Command("1984cmd")]
     [Summary("Disable a command.")]
-    [Remarks("$disablecmd [cmd]")]
+    [Remarks("$1984cmd [cmd]")]
     public async Task<RuntimeResult> DisableCommand(string cmd)
     {
         string cmdLower = cmd.ToLower();
-        if (cmdLower == "disablecmd")
+        if (cmdLower == "ban")
             return CommandResult.FromError("I don't think that's a good idea.");
 
         Discord.Commands.SearchResult search = Commands.Search(cmd);
@@ -60,50 +52,6 @@ public class BotOwner : ModuleBase<SocketCommandContext>
         globalConfig.DisabledCommands.Add(cmdLower);
         await Context.User.NotifyAsync(Context.Channel, $"Disabled ${cmdLower}.");
         return CommandResult.FromSuccess();
-    }
-
-    [Command("enablecmd")]
-    [Summary("Enable a previously disabled command.")]
-    [Remarks("$enablecmd [cmd]")]
-    public async Task<RuntimeResult> EnableCommand(string cmd)
-    {
-        string cmdLower = cmd.ToLower();
-        DbGlobalConfig globalConfig = await DbGlobalConfig.Get();
-        if (!globalConfig.DisabledCommands.Contains(cmdLower))
-            return CommandResult.FromError($"**{cmdLower}** is either not a command or is not disabled!");
-
-        globalConfig.DisabledCommands.Remove(cmdLower);
-        await Context.User.NotifyAsync(Context.Channel, $"Enabled ${cmdLower}.");
-        return CommandResult.FromSuccess();
-    }
-
-    [Alias("evaluate")]
-    [Command("eval")]
-    [Summary("Execute C# code.")]
-    [Remarks("$eval [code]")]
-    public async Task<RuntimeResult> Eval([Remainder] string code)
-    {
-        try
-        {
-            code = code.Replace("```cs", "").Trim('`');
-            string[] imports = { "System", "System.Collections.Generic", "System.Text" };
-            object evaluation = await CSharpScript.EvaluateAsync(code, ScriptOptions.Default.WithImports(imports), new FunnyContext(Context));
-
-            EmbedBuilder embed = new EmbedBuilder()
-                .WithColor(Color.Red)
-                .WithTitle("Code Evaluation")
-                .WithDescription($"Your code, ```cs\n{code}``` evaluates to: ```cs\n\"{evaluation}\"```");
-            await ReplyAsync(embed: embed.Build());
-            return CommandResult.FromSuccess();
-        }
-        catch (CompilationErrorException cee)
-        {
-            return CommandResult.FromError($"Compilation error: ``{cee.Message}``");
-        }
-        catch (Exception e) when (e is not NullReferenceException)
-        {
-            return CommandResult.FromError($"Other error: ``{e.Message}``");
-        }
     }
 
     [Alias("setuserproperty")]
@@ -123,21 +71,6 @@ public class BotOwner : ModuleBase<SocketCommandContext>
         {
             return CommandResult.FromError($"Couldn't set property: {e.Message}");
         }
-    }
-
-    [Alias("unbotban")]
-    [Command("unblacklist")]
-    [Summary("Unban a user from using the bot.")]
-    [Remarks("$unblacklist [user]")]
-    public async Task<RuntimeResult> Unblacklist(IGuildUser user)
-    {
-        if (user.IsBot)
-            return CommandResult.FromError("Nope.");
-
-        DbGlobalConfig globalConfig = await DbGlobalConfig.Get();
-        globalConfig.BannedUsers.Remove(user.Id);
-        await Context.User.NotifyAsync(Context.Channel, $"Unblacklisted {user.Sanitize()}.");
-        return CommandResult.FromSuccess();
     }
 
     [Command("updatedb")]
