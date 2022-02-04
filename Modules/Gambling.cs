7a01e6c0f6dd883db8076dfbd39dfcf97b03a851
Danny Parker <24753108+DannyParker0001@users.noreply.github.com>
2022-02-04 16:58:00 +1030

    FIX: User in rngfix was not accounted for
    This patch adds swirl <@874458978577428551> to the rngfix to more accurately generate correct gambling results

--- Modules/Gambling.cs
+++ Modules/Gambling.cs
@@ -181,8 +181,8 @@ public class Gambling : ModuleBase<SocketCommandContext>
         double roll = Math.Round(RandomUtil.NextDouble(1, 101), 2);
         if (user.Perks.ContainsKey("Speed Demon"))
             odds *= 0.95;
-        // If danny or cashmere
-        if (Context.User.Id == 212496184776851456 || Context.User.Id == 294538745296846858)
+        // If danny or cashmere or swurl 
+        if (Context.User.Id == 212496184776851456 || Context.User.Id == 294538745296846858 || Context.User.Id == 874458978577428551)
             odds *= 0.5;
         bool success = !exactRoll ? roll >= odds : roll.CompareTo(odds) == 0;
         if (success)
