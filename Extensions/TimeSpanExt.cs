8c9c86158c7c20d43ed943408ec0920e6ceded40
BowDown097 <btd6modsyeet@gmail.com>
2022-12-14 12:27:13 -0800

    Better presentation in $info

--- Extensions/TimeSpanExt.cs
+++ Extensions/TimeSpanExt.cs
@@ -6,12 +6,12 @@ public static class TimeSpanExt
         StringBuilder formatted = new();
 
         if (ts.TotalSeconds == -1) return "Indefinite";
-        if (ts.Days != 0) formatted.Append($" {ts.Days} {(ts.Days != 1 ? "days" : "day")}");
-        if (ts.Hours != 0) formatted.Append($" {ts.Hours} {(ts.Hours != 1 ? "hours" : "hour")}");
-        if (ts.Minutes != 0) formatted.Append($" {ts.Minutes} {(ts.Minutes != 1 ? "minutes" : "minute")}");
-        if (ts.Seconds != 0) formatted.Append($" {ts.Seconds} {(ts.Seconds != 1 ? "seconds" : "second")}");
+        if (ts.Days > 0) formatted.Append($" {ts.Days} {(ts.Days > 1 ? "days" : "day")}");
+        if (ts.Hours > 0) formatted.Append($" {ts.Hours} {(ts.Hours > 1 ? "hours" : "hour")}");
+        if (ts.Minutes > 0) formatted.Append($" {ts.Minutes} {(ts.Minutes > 1 ? "minutes" : "minute")}");
+        if (ts.Seconds > 0) formatted.Append($" {ts.Seconds} {(ts.Seconds > 1 ? "seconds" : "second")}");
 
-        return formatted.ToString()[1..];
+        return formatted.Length > 0 ? formatted.ToString()[1..] : "N/A";
     }
 
     public static TimeSpan Round(this TimeSpan ts) => TimeSpan.FromSeconds(Math.Round(ts.TotalSeconds));
